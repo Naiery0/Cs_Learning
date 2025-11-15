886674c3fbd5c819d8cea858418e9b1d37f31b72
Naiery0 <molly724@naver.com>
2025-11-16 02:13:28 +0900

    3차 commit

--- Cs_Toy_remindC/Cs_Toy_remindC/ViewModels/GameViewModel.cs
+++ Cs_Toy_remindC/Cs_Toy_remindC/ViewModels/GameViewModel.cs
@@ -4,11 +4,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Cs_Toy_remindC.ViewModels
 {
     public partial class GameViewModel : ObservableObject
     {
-
+        //public GameViewModel() { MessageBox.Show("테스트"); }
     }
 }
--- Cs_Toy_remindC/Cs_Toy_remindC/ViewModels/HomeViewModel.cs
+++ Cs_Toy_remindC/Cs_Toy_remindC/ViewModels/HomeViewModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+// 이거 필요함?
 namespace Cs_Toy_remindC.ViewModels
 {
     class HomeViewModel
